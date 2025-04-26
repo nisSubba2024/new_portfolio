9e93580dde655e56793c37e83bd422cc659d777d
Nishan Subba <nishanlimsubba2100@gmail.com>
2025-04-25 20:34:58 -0400

    projects update

--- src/navigation/Navigation.jsx
+++ src/navigation/Navigation.jsx
@@ -27,6 +27,7 @@ export function NavBar({menuChange, onLinkClick}) {
 export default function Navigation() {
     const [menuActive, setMenuActive] = useState(false);
     const [darkMode, setDarkMode] = useState(false);
+
     function toggleMenuIcon() {
         setMenuActive((prevState) => !prevState);
     }
@@ -45,7 +46,7 @@ export default function Navigation() {
         } else {
             document.body.classList.remove('dark-mode');
         }
-    },[darkMode])
+    }, [darkMode])
 
     return (
         <>
@@ -53,13 +54,15 @@ export default function Navigation() {
                 <div className="logo">
                     <a href={"#home"} className={"link logo-active"}>Nishan Subba</a>
                     <a href={"#home"} className={"link logo-hidden"}>NS</a>
+
                 </div>
                 <nav className={`nav-menu`}>
                     <NavBar menuChange={menuActive} onLinkClick={closeNavBar}/>
                     <i onClick={toggleDarkMode} className="fa-solid fa-circle-half-stroke mode-switcher"></i>
                     <i onClick={toggleMenuIcon} id="menu-icon" className="fa-solid fa-bars"></i>
                     <div className="visit-github">
-                        <a href={"https://github.com/nisSubba2024"} target={"_blank"} className={"link btn github-btn"}>Visit Github
+                        <a href={"https://github.com/nisSubba2024"} target={"_blank"} className={"link btn github-btn"}>Visit
+                            Github
                         </a>
                     </div>
                 </nav>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,4 +1,6 @@
 import UnscrambledImg from '../assets/unscramble_me.png';
+import ParkingPaymentDB from '../assets/project_ss/er-diagram.png';
+
 
 function ProjectGroup({groupName, projectDetails}) {
     return (
@@ -21,12 +23,22 @@ function CreateCard({projectDetails}) {
                     <div key={index} className={"project-card"}>
                         <img className={"project-img"} src={projectDetails[key].imgSrc} alt={projectDetails[key].imgAlt}/>
                         <h3 className={"project-title"}>{projectDetails[key].name}</h3>
+                        {projectDetails[key].techStacks && (
+                            <div className={"project-tech-group"}>
+                                {Object.keys(projectDetails[key].techStacks).map((tech, index) => (
+                                    <p key={index}
+                                       className={"tech-stack-wrapper"}>{projectDetails[key].techStacks[tech]}</p>
+                                ))}
+
+                            </div>
+
+                        )}
                         <p className={"project-description"}>{projectDetails[key].description}
                         </p>
                         <div className={"project-btn-group"}>
                             {
-                                Object.keys(projectDetails[key].button).map((btn, index) => (
-                                    <a href={"#"} key={index} className={"link btn"}>{projectDetails[key].button[btn]}</a>
+                                Object.entries(projectDetails[key].button).map(([btn, link], index) => (
+                                    <a target={"_blank"} href={link} key={index} className={"link btn"}>{btn}</a>
                                 ))
                             }
                         </div>
@@ -45,22 +57,49 @@ export default function Projects() {
                 imgSrc: UnscrambledImg,
                 imgAlt: "Unscrambled Me",
                 name: 'Unscrambled Me1',
+                techStacks: ['SQL', 'PL/SQL', 'Oracle 10g'],
                 description: 'This is a testing project and will be deleted promptly.',
-                button: ['Live Demo', 'Source Code']
+                button: {
+                    'Live Demo': '#',
+                    'GitHub Repo': '#'
+                }
             },
             unscrambled2: {
                 imgSrc: UnscrambledImg,
                 imgAlt: "Unscrambled Me",
                 name: 'Unscrambled Me2',
                 description: 'This is a testing project and will be deleted promptly.',
-                button: ['GitHub Repo']
+                button: {
+                    'Live Demo': '#',
+                    'GitHub Repo': '#'
+                }
+                // button: ['GitHub Repo']
             },
             unscrambled3: {
                 imgSrc: UnscrambledImg,
                 imgAlt: "Unscrambled Me",
                 name: 'Unscrambled Me3',
                 description: 'This is a testing project and will be deleted promptly.',
-                button: ['Live Demo', 'GitHub Repo']
+                button: {
+                    'Live Demo': '#',
+                    'GitHub Repo': '#'
+                }
+                // button: ['Live Demo', 'GitHub Repo']
+            }
+        },
+        databaseDevelopment: {
+            parkingPayment: {
+                imgSrc: ParkingPaymentDB,
+                imgAlt: "Er diagram of Parking Payment System",
+                name: 'Parking Payment DB Management',
+                techStacks: ['SQL', 'PL/SQL', 'Oracle 10g'],
+                description: 'UMBC Capstone project. This Parking Payment Management System allows users to register' +
+                    ' vehicles,' +
+                    ' find available parking zones, manage sessions, receive expiration alerts, and view detailed session reports including duration and cost.',
+                button: {
+                    'GitHub Repo': 'https://github.com/nisSubba2024/payment_management_system'
+                }
+                // button: ['Source Code']
             }
         }
     }
@@ -73,6 +112,8 @@ export default function Projects() {
                 <div className={"projects-group"}>
                     <ProjectGroup groupName={'JavaScript Project'} projectDetails={projectDetails.webDev}/>
                     <ProjectGroup groupName={'C++ Project'} projectDetails={projectDetails.webDev}/>
+                    <ProjectGroup groupName={'Database Management'}
+                                  projectDetails={projectDetails.databaseDevelopment}/>
                 </div>
             </div>
 
